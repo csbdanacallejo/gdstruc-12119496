38f4fa29b8105649c5e63076141c561ef880ca45
Callejo_Dana_Andrea <danaandrea.callejo@benilde.edu.ph>
2022-11-28 01:36:44 +0800

    Answers for Quiz 6

---
+++ GDSTRUC-OTIE2-20221-Quiz6/src/gdstruc/module7/Main.java
@@ -0,0 +1,26 @@
+package gdstruc.module7;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        tree.insert(25);
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(27);
+        tree.insert(30);
+        tree.insert(29);
+        tree.insert(26);
+        tree.insert(22);
+        tree.insert(32);
+
+        // traverse the tree (in-order)
+        //tree.traverseInOrder();
+
+        // search a node by value
+        //System.out.println(tree.get(32));
+        tree.traverseInOrderDescending();
+        tree.getMin();
+        tree.getMax();
+    }
+}
---
+++ GDSTRUC-OTIE2-20221-Quiz6/src/gdstruc/module7/Node.java
@@ -0,0 +1,126 @@
+package gdstruc.module7;
+
+public class Node {
+    private int data;
+    private Node rightChild;
+    private Node leftChild;
+
+    public Node(int _data) {
+        data = _data;
+    }
+
+    public void insert(int value) {
+        if (value == data) {
+            return;
+        }
+
+        if (value < data) {     // left subtree
+            if (leftChild == null) {
+                leftChild = new Node(value);
+            }
+            else {
+                leftChild.insert(value);
+            }
+        }
+        else {                  // right subtree
+            if (rightChild == null) {
+                rightChild = new Node(value);
+            }
+            else {
+                rightChild.insert(value);
+            }
+        }
+    }
+    public void traverseInOrder() {
+        // left subtree
+        if (leftChild != null) {
+            leftChild.traverseInOrder();
+        }
+        // print current node (root or subtree)
+        System.out.println("Data: " + data);
+
+        // right subtree
+        if (rightChild != null) {
+            rightChild.traverseInOrder();
+        }
+    }
+    public void traverseInOrderDescending() {
+        //Reversed the order of the traverse/checking so that it starts at right tree
+
+        // right subtree
+        if (rightChild != null) {
+            rightChild.traverseInOrderDescending();
+        }
+        // print current node (root or subtree)
+        System.out.println("Data: " + data);
+
+        // left subtree
+        if (leftChild != null) {
+            leftChild.traverseInOrderDescending();
+        }
+    }
+    public void getMin() {
+        // checks left subtree since all values on that side are lower than root, making it easier to find min value
+        if (leftChild != null) {
+            leftChild.traverseInOrderDescending();
+            System.out.println(leftChild);
+        }
+    }
+    public void getMax() {
+        // checks right subtree since all values on that side are higher than root, making it easier to find max value
+        if (rightChild != null) {
+            rightChild.traverseInOrder();
+            System.out.println(rightChild);
+        }
+    }
+
+    public Node get(int value) {
+        if (value == data) {
+            return this;
+        }
+
+        if (value < data) {
+            if (leftChild != null) {
+                return  leftChild.get(value);
+            }
+        }
+        else {
+            if (rightChild != null) {
+                return  rightChild.get(value);
+            }
+        }
+
+        return null;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                '}';
+    }
+}
